8ca64c372723a71321ead9c3890e5bddd412428b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-25 15:06:27 -0400

    Add parent/child

---
+++ src/components/Parent.tsx
@@ -0,0 +1,9 @@
+import { Child } from "./Child";
+
+export const Parent = () => {
+    return (
+        <>
+            <Child color="blue" />
+        </>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom";
+import { Parent } from "./components/Parent";
 
 const App = () => {
     return (
-        <div>
+        <>
             <h1>React Typescript Project 1</h1>
-        </div>
+            <Parent />
+        </>
     );
 };
 
