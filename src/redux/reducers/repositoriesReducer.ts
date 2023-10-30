323c5890601b2b26d0fa15ad00e6f55e5528af3b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-30 15:57:09 -0400

    Use RepositoriesState

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -10,11 +10,29 @@ interface RepositoriesState {
     data: string[];
 }
 
-const repositoriesReducer = (state: RepositoriesState, action: any) => {
+const repositoriesReducer = (
+    state: RepositoriesState,
+    action: any
+): RepositoriesState => {
     switch (action.type) {
         case actionTypes.search:
+            return {
+                loading: true,
+                error: null,
+                data: [],
+            };
         case actionTypes.success:
+            return {
+                loading: false,
+                error: null,
+                data: action.payload,
+            };
         case actionTypes.error:
+            return {
+                loading: false,
+                error: action.payload,
+                data: [],
+            };
         default:
             return state;
     }
