a63862889144a1aa029578e3d291f43f4d59a044
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-30 16:40:41 -0400

    Use Redux Toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import { Reservations } from "./components/Reservations";
+import { store } from "./redux";
 
 const App = () => {
     return <Reservations />;
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector("#root")
+);
--- src/redux/actions/repositoriesActions.ts
+++ src/redux/actions/repositoriesActions.ts
@@ -1,10 +1,26 @@
 import axios from "axios";
+
 import { actionTypes } from "../reducers/repositoriesReducer";
+import { bu } from "../../a";
 
 export const searchRepositoriesAction = (term: string) => {
     return async (dispatch: any) => {
         dispatch({
             type: actionTypes.search,
         });
+
+        try {
+            const result = await axios.get(bu, {
+                params: {
+                    text: term,
+                },
+            });
+            console.log(result.data);
+        } catch (err: any) {
+            dispatch({
+                type: actionTypes.error,
+                payload: err.message,
+            });
+        }
     };
 };
