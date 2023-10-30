d60f807ce711bea0080a12c9f5c7dbbea42d13ec
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-10-30 15:54:03 -0400

    Add action types

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -1,9 +1,23 @@
+export const actionTypes = {
+    search: "SEARCH",
+    success: "SUCCESS",
+    error: "ERROR",
+};
+
 interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
 }
 
-const repositoriesReducer = (state: RepositoriesState, action: any) => {};
+const repositoriesReducer = (state: RepositoriesState, action: any) => {
+    switch (action.type) {
+        case actionTypes.search:
+        case actionTypes.success:
+        case actionTypes.error:
+        default:
+            return state;
+    }
+};
 
 export default repositoriesReducer;
